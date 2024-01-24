fd6813368bebab59f8c5f6cde8b61d708326b7dc
Prakash-102 <142473108+Prakash-102@users.noreply.github.com>
2024-01-24 19:02:56 +0530

    Add files via upload

---
+++ CatalanTab.java
@@ -0,0 +1,28 @@
+
+//Wildcard mathching-hard(Explanation):-
+import java.util.*;
+
+public class CatalanTab {
+
+    public static int calalanTab(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(calalanTab(n));
+    }
+}
+
+/*
+ * Output:-
+ * 14
+ */
